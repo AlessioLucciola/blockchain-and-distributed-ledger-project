1a817cab2fd748369e426ab01793166cb86a517e
AlessioLucciola <alessiolucciola98@hotmail.it>
2023-12-14 17:04:55 +0100

    Added some functions to Entities.sol to manage verification

--- backend/contracts/Entities.sol
+++ backend/contracts/Entities.sol
@@ -9,6 +9,9 @@ contract Roles {
     mapping(address => bool) public distributors;
     mapping(address => bool) public retailers;
     mapping(address => bool) public customers;
+
+    // Define a set of mappings to manage the verification process
+    mapping(address => bool) public entityVerificationPermission;
     mapping(address => bool) public verificationStatus;
 
     // Define a set of events triggered when adding/removing an entity
@@ -27,28 +30,34 @@ contract Roles {
         _;
     }
 
-    // Modifier function to allow other functions to be executed only by manufacturers
+    // Modifier function to allow other functions to be executed only by manufacturers or the admin
     modifier onlyManufacturerAdmin() {
         require(manufacturers[msg.sender] || msg.sender == admin, "Only manufacturers or the admin can perform this action");
         _;
     }
 
-    // Modifier function to allow other functions to be executed only by distributors
+    // Modifier function to allow other functions to be executed only by distributors or the admin
     modifier onlyDistributorAdmin() {
         require(distributors[msg.sender] || msg.sender == admin, "Only distributors or the admin can perform this action");
         _;
     }
 
-    // Modifier function to allow other functions to be executed only by retailers
+    // Modifier function to allow other functions to be executed only by retailers or the admin
     modifier onlyRetailerAdmin() {
         require(retailers[msg.sender] || msg.sender == admin, "Only retailers or the admin can perform this action");
         _;
     }
 
-    // Modifier function to allow other functions to be executed if the account is verified
-    modifier onlyVerified(address account) {
-        require(msg.sender == admin, "Only an admin can force the verification removal");
-        require(verificationStatus[account], "Entity is not yet verified");
+    // Modifier function to allow other functions to be executed only by customers or the admin
+    modifier onlyCustomerAdmin() {
+        require(customers[msg.sender] || msg.sender == admin, "Only customers or the admin can perform this action");
+        _;
+    }
+
+    // Modifier function that check if the entity has sent the required proof to get the verified badge
+    modifier onlyVerificationPermittedEntity() {
+        require(entityVerificationPermission[msg.sender], "Entity hasn't given its identification proof yet");
+        require(!customers[msg.sender], "Customers are not allowed to perform this action");
         _;
     }
 
@@ -65,6 +74,7 @@ contract Roles {
     // Define a function to add a new Manufacturer
     function addManufacturer() external {
         manufacturers[msg.sender] = true;
+        entityVerificationPermission[msg.sender] = false;
         verificationStatus[msg.sender] = false;
         emit ManufacturerAdded(msg.sender);
     }
@@ -72,6 +82,7 @@ contract Roles {
     // Define a function to remove a Manufacturer
     function removeManufacturer() external onlyManufacturerAdmin {
         manufacturers[msg.sender] = false;
+        entityVerificationPermission[msg.sender] = false;
         verificationStatus[msg.sender] = false;
         emit ManufacturerRemoved(msg.sender);
     }
@@ -84,6 +95,7 @@ contract Roles {
     // Define a function to add a new Distibutor
     function addDistributors() external {
         distributors[msg.sender] = true;
+        entityVerificationPermission[msg.sender] = false;
         verificationStatus[msg.sender] = false;
         emit DistributorAdded(msg.sender);
     }
@@ -91,6 +103,7 @@ contract Roles {
     // Define a function to remove a Distributor
     function removeDistributor() external onlyDistributorAdmin {
         distributors[msg.sender] = false;
+        entityVerificationPermission[msg.sender] = false;
         verificationStatus[msg.sender] = false;
         emit DistributorRemoved(msg.sender);
     }
@@ -103,6 +116,7 @@ contract Roles {
     // Define a function to add a new Retailer
     function addRetailer() external {
         retailers[msg.sender] = true;
+        entityVerificationPermission[msg.sender] = false;
         verificationStatus[msg.sender] = false;
         emit RetailerAdded(msg.sender);
     }
@@ -110,6 +124,7 @@ contract Roles {
     // Define a function to remove a Retailer
     function removeRetailer() external onlyRetailerAdmin {
         retailers[msg.sender] = false;
+        entityVerificationPermission[msg.sender] = false;
         verificationStatus[msg.sender] = false;
         emit RetailerRemoved(msg.sender);
     }
@@ -131,7 +146,28 @@ contract Roles {
         emit CustomerRemoved(msg.sender);
     }
 
-    function verifyEntity() external payable onlyAdmin {
+    // Define a function to check if the account sent the identification proof and it was accepted by an admin
+    function isVerificationPermitted(address account) public view returns (bool) {
+        return entityVerificationPermission[account];
+    }
+
+    // Allow admin to grant permission for an entity to trigger verification
+    function grantVerificationPermission(address account) external onlyAdmin {
+        entityVerificationPermission[account] = true;
+    }
+
+    // Allow admin to revoke permission for an entity to trigger verification
+    function revokeVerificationPermission(address account) external onlyAdmin {
+        entityVerificationPermission[account] = false;
+    }
+
+    // Define a function to check if the account is verified
+    function isVerified(address account) public view returns (bool) {
+        return verificationStatus[account];
+    }
+
+    // Define a function to make the payment and receive the verified badge (only for entities that sent a proof and it was accepted by the admin)
+    function verifyEntity() external payable onlyVerificationPermittedEntity {
         // Ensure a certain amount of Ether is sent for verification
         require(msg.value >= 0.2 ether, "Insufficient payment for verification");
 
@@ -140,7 +176,8 @@ contract Roles {
     }
 
     // Define a function to force the removal of the verification (can be done only by the admin)
-    function removeVerification(address account) external onlyVerified {
+    function removeVerification(address account) external onlyAdmin {
+        require(verificationStatus[account], "Entity is already not verified");
         verificationStatus[account] = false;
     }
 }
