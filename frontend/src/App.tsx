24d10044d5f6cb5c08b682124bc41cd9981967d9
Anomalyzed <corsi.1742375@studenti.uniroma1.it>
2023-12-20 21:23:33 +0100

    Connection with metamask wallet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+declare global {
+  interface Window {
+    ethereum?: any;
+  }
+}
+
+const App = () => {
+  const [balance, setBalance] = useState<number | null>(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const [walletAddress, setWalletAddress] = useState<string | null>(null); // Added state for wallet address
+  const [isMetamaskInstalled, setIsMetamaskInstalled] = useState(false); // Added state for checking if Metamask is installed
+
+  useEffect(() => {
+    checkMetamaskInstalled();
+  }, []);
+
+  const checkMetamaskInstalled = () => {
+    setIsMetamaskInstalled(!!window.ethereum);
+  };
+
+  const connectWallet = async () => {
+    try {
+      // Connect to Metamask wallet
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      setIsConnected(true);
+      const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+      setWalletAddress(accounts[0]); // Set the wallet address
+    } catch (error) {
+      console.error('Failed to connect wallet:', error);
+    }
+  };
+
+  const getBalance = async () => {
+    try {
+      // Get the balance from Metamask wallet
+      const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+      const balance = await window.ethereum.request({
+        method: 'eth_getBalance',
+        params: [accounts[0], 'latest'],
+      });
+      setBalance(parseInt(balance));
+    } catch (error) {
+      console.error('Failed to get balance:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Metamask Wallet Balance</h1>
+      {isMetamaskInstalled ? (
+        isConnected ? (
+          <div>
+            <p>Connected to Metamask wallet</p>
+            <p>Wallet Address: {walletAddress}</p> {/* Display the wallet address */}
+            <button onClick={getBalance}>Get Balance</button>
+            {balance !== null && <p>Balance: {balance} ETH</p>}
+          </div>
+        ) : (
+          <button onClick={connectWallet}>Connect Wallet</button>
+        )
+      ) : (
+        <p>Metamask is not installed</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
