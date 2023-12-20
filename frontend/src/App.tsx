c3215e1d27a623833ff841bdc63b93dbdbb42f69
Anomalyzed <corsi.1742375@studenti.uniroma1.it>
2023-12-20 21:51:32 +0100

    Testing sending transaction

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,40 @@
 import React, { useState, useEffect } from 'react';
 
+/**
+ * Interface for the global window object to declare the ethereum property.
+ */
 declare global {
   interface Window {
     ethereum?: any;
   }
 }
 
+/**
+ * Main component for the Metamask Wallet Balance application.
+ */
 const App = () => {
   const [balance, setBalance] = useState<number | null>(null);
   const [isConnected, setIsConnected] = useState(false);
   const [walletAddress, setWalletAddress] = useState<string | null>(null); // Added state for wallet address
   const [isMetamaskInstalled, setIsMetamaskInstalled] = useState(false); // Added state for checking if Metamask is installed
 
+  /**
+   * Checks if Metamask is installed in the browser.
+   */
   useEffect(() => {
     checkMetamaskInstalled();
   }, []);
 
+  /**
+   * Checks if Metamask is installed in the browser.
+   */
   const checkMetamaskInstalled = () => {
     setIsMetamaskInstalled(!!window.ethereum);
   };
 
+  /**
+   * Connects to the Metamask wallet.
+   */
   const connectWallet = async () => {
     try {
       // Connect to Metamask wallet
@@ -32,6 +47,18 @@ const App = () => {
     }
   };
 
+  /**
+   * Disconnects from the Metamask wallet.
+   */
+  const disconnectWallet = () => {
+    setIsConnected(false);
+    setWalletAddress(null);
+    setBalance(null);
+  };
+
+  /**
+   * Gets the balance from the Metamask wallet.
+   */
   const getBalance = async () => {
     try {
       // Get the balance from Metamask wallet
@@ -46,6 +73,35 @@ const App = () => {
     }
   };
 
+  /**
+   * Sends ether to another wallet.
+   */
+  const sendEther = async () => {
+    try {
+      // Send ether to another wallet
+      const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+      await window.ethereum.request({
+        method: 'eth_sendTransaction',
+        params: [
+          {
+            from: accounts[0], // Sender's address
+            to: '0xBB49086f7463178Be99908eba5a6cAE21b188564', // Receiver's address (SmartSupply)
+            value: '0x29a2241af62c0000', // 0.1 ETH
+          },
+        ],
+      });
+    } catch (error) {
+      console.error('Failed to send ether:', error);
+    }
+  };
+
+  /**
+   * Fetches the initial balance when the component is mounted.
+   */
+  useEffect(() => {
+    getBalance(); // Call getBalance function to fetch the initial balance
+  }, []);
+
   return (
     <div>
       <h1>Metamask Wallet Balance</h1>
@@ -56,6 +112,8 @@ const App = () => {
             <p>Wallet Address: {walletAddress}</p> {/* Display the wallet address */}
             <button onClick={getBalance}>Get Balance</button>
             {balance !== null && <p>Balance: {balance} ETH</p>}
+            <button onClick={sendEther}>Send Ether</button>
+            <button onClick={disconnectWallet}>Disconnect Wallet</button> {/* Added disconnect wallet button */}
           </div>
         ) : (
           <button onClick={connectWallet}>Connect Wallet</button>
