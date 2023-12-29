3977c8236727322f3c6d10faf320092304969204
Anomalyzed <corsi.1742375@studenti.uniroma1.it>
2023-12-29 21:24:59 +0100

    Testing simple function of smartsupply contract

--- backend/scripts/deploy.ts
+++ backend/scripts/deploy.ts
@@ -1,11 +1,19 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  // const smartsupply = await ethers.deployContract("SmartSupply", ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", "0x70997970C51812dc3A010C7d01b50e0d17dc79C8", "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"]);
-  const contract = await ethers.deployContract("Entities"); // Deploying the contract
-  await contract.waitForDeployment(); // Waiting for the deployment to finish
+  const BalanceManager = await ethers.deployContract("BalanceManager");
+  await BalanceManager.waitForDeployment();
+  
+  const Entities = await ethers.deployContract("Entities");
+  await Entities.waitForDeployment();
 
-  console.log("Contract deployed to:", contract.target); // Prints the address where the contract is deployed
+  const SmartSupply = await ethers.deployContract("SmartSupply");
+  await SmartSupply.waitForDeployment();
+
+  const Utils = await ethers.deployContract("Utils");
+  await Utils.waitForDeployment();
+
+  console.log("SmartSupply deployed to:", SmartSupply.target);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
