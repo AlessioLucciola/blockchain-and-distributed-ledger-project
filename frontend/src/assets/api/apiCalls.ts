fe95fb71dc55ce1f5c7931970bc76762a4411ab0
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-12-30 16:03:29 +0100

    apis: added basic apis on frontend

---
+++ frontend/src/assets/api/apiCalls.ts
@@ -0,0 +1,41 @@
+import axios from "axios"
+import { Entity } from "../../shared/types"
+
+export const api = axios.create({
+	baseURL: "http://localhost:3000/api",
+})
+
+export const getEntities = async ({ role }: { role?: string }) => {
+	const { data } = await api.get("/get-entities", { params: { role } })
+	return data
+}
+
+export const createEntity = async ({ name, surname, email, password, address_1, address_2, companyName, shopName, metamaskAddress, role }: Entity) => {
+	const { data } = await api.post("/create-entity", {
+		name,
+		surname,
+		email,
+		password,
+		address_1,
+		address_2,
+		companyName,
+		shopName,
+		metamaskAddress,
+		role,
+	})
+	return data
+}
+export const deleteEntity = async ({ id }: { id: string }) => {
+	const { data } = await api.delete("/delete-entity", { params: { id } })
+	return data
+}
+
+export const addProduct = async ({ name, description }: { name: string; description: string }) => {
+	const { data } = await api.post("/add-product", { name, description })
+	return data
+}
+
+export const addProductInstance = async ({ productInstanceId, productId }: { productInstanceId: string; productId: string }) => {
+	const { data } = await api.post("/add-product-instance", { productInstanceId, productId })
+	return data
+}
--- frontend/src/shared/types.ts
+++ frontend/src/shared/types.ts
@@ -1,5 +1,33 @@
+import { Roles } from "./constants"
+
 export type SearchResult = {
 	id: string
 	name: string
 	image: string
+}
+
+export type Entity = {
+	id?: string
+	name: string
+	surname: string
+	email: string
+	password: string
+	address_1: string
+	address_2?: string
+	companyName?: string
+	shopName?: string
+	metamaskAddress: string
+	role: Roles
+}
+
+export type Product = {
+	uid?: string
+	name: string
+	description: string
+	productInstances: ProductInstance[]
+}
+
+export type ProductInstance = {
+	id?: string
+	productId: string
 }
