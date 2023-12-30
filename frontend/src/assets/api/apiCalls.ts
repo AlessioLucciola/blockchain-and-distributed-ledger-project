9bf45f6ae7de7278c5b45ae31dc0db66a3fafccc
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-12-30 17:46:43 +0100

    frontend: search for product

--- frontend/src/assets/api/apiCalls.ts
+++ frontend/src/assets/api/apiCalls.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from "axios"
-import { Entity } from "../../shared/types"
+import { Entity, Product } from "../../shared/types"
 
 export const api = axios.create({
 	baseURL: "http://localhost:3000/api",
@@ -10,7 +10,18 @@ export const getEntities = async ({ role }: { role?: string }): Promise<AxiosRes
 	return res
 }
 
-export const createEntity = async ({ name, surname, email, password, address_1, address_2, companyName, shopName, metamaskAddress, role }: Entity): Promise<AxiosResponse<{ message: string }>> => {
+export const createEntity = async ({
+	name,
+	surname,
+	email,
+	password,
+	address_1,
+	address_2,
+	companyName,
+	shopName,
+	metamaskAddress,
+	role,
+}: Entity): Promise<AxiosResponse<{ message: string; data: Entity }>> => {
 	const res = await api.post("/create-entity", {
 		name,
 		surname,
@@ -25,21 +36,30 @@ export const createEntity = async ({ name, surname, email, password, address_1, 
 	})
 	return res
 }
-export const deleteEntity = async ({ id }: { id: string }): Promise<AxiosResponse<{ message: string }>> => {
+export const deleteEntity = async ({ id }: { id: string }): Promise<AxiosResponse<{ message: string; data: Entity }>> => {
 	const res = await api.delete("/delete-entity", { params: { id } })
 	return res
 }
 
-export const addProduct = async ({ name, description }: { name: string; description: string }): Promise<AxiosResponse<{ message: string }>> => {
+export const addProduct = async ({ name, description }: { name: string; description: string }): Promise<AxiosResponse<{ message: string; data: Product }>> => {
 	const res = await api.post("/add-product", { name, description })
 	return res
 }
 
-export const addProductInstance = async ({ productInstanceId, productId, soldBy }: { productInstanceId: string; productId: string; soldBy: number }): Promise<AxiosResponse<{ message: string }>> => {
+export const addProductInstance = async ({
+	productInstanceId,
+	productId,
+	soldBy,
+}: {
+	productInstanceId: string
+	productId: string
+	soldBy: number
+}): Promise<AxiosResponse<{ message: string; data: any }>> => {
+	//TODO: change any to correct type
 	const res = await api.post("/add-product-instance", { productInstanceId, productId, soldBy })
 	return res
 }
-export const searchProduct = async ({ name }: { name: string }): Promise<AxiosResponse<{ message: string }>> => {
+export const searchProduct = async ({ name }: { name: string }): Promise<AxiosResponse<{ message: string; data: Product[] }>> => {
 	const res = await api.get("/search-product", { params: { name } })
 	return res
 }
